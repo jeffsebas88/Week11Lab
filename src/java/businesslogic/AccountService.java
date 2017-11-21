0fbc89ba07562362567d7addded11f8ff5f4d1f4
643507 <643507@ELF6IT216349.ACDM.DS.SAIT.CA>
2017-11-21 10:27:01 -0700

    Week 11 Lab got to String email in User service
    Jeff Sebastian

---
+++ src/java/businesslogic/AccountService.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package businesslogic;
+
+import dataaccess.NotesDBException;
+import dataaccess.UserDB;
+import domainmodel.*;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.MessagingException;
+import javax.naming.NamingException;
+
+/**
+ *
+ * @author awarsyle
+ */
+public class AccountService {
+    
+    public User checkLogin(String username, String password, String path) {
+        User user;
+        
+        UserDB userDB = new UserDB();
+        try {
+            user = userDB.getUser(username);
+            
+            if (user.getPassword().equals(password)) {
+                // successful login
+                Logger.getLogger(AccountService.class.getName())
+                        .log(Level.INFO,
+                                "A user logged in: {0}", username);
+                String email = user.getEmail();
+                try {
+                    // WebMailService.sendMail(email, "NotesKeepr Login", "Big brother is watching you!  Hi " + user.getFirstname(), false);
+                    
+                    HashMap<String, String> contents = new HashMap<>();
+                    contents.put("firstname", user.getFirstname());
+                    contents.put("date", ((new java.util.Date()).toString()));
+                    
+                    try {
+                        WebMailService.sendMail(email, "NotesKeepr Login", path + "/emailtemplates/login.html", contents);
+                    } catch (IOException ex) {
+                        Logger.getLogger(AccountService.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    
+                    
+                } catch (MessagingException ex) {
+                    Logger.getLogger(AccountService.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (NamingException ex) {
+                    Logger.getLogger(AccountService.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
+                return user;
+            }
+            
+        } catch (NotesDBException ex) {
+        }
+        
+        return null;
+    }
+    private boolean forgotPassword(String email, String path)
+    {
+        UserService us = new UserService();
+        
+        
+        return true;
+}
+}
---
+++ web/WEB-INF/forgot.jsp
@@ -0,0 +1,22 @@
+<%-- 
+    Document   : forgot
+    Created on : Nov 21, 2017, 10:20:55 AM
+    Author     : 643507
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Password</title>
+    </head>
+    <body>
+        <h1>Forgot Password</h1>
+        <p>Please enter your email address to retrieve your password</p>
+        <form action="forgot?action=forgot" method="post">
+        Email Address: <input type="text" name="email"><br>
+        <input type="submit" value="Submit">
+</form>
+    </body>
+</html>
